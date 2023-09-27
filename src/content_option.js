1f54f16499a3f16713a590aaad1a39e3e9f49d51
Aveeral Gaur <agaur@macs-MacBook-Pro.local>
2023-09-27 22:59:08 +0530

    First commit

---
+++ src/content_option.js
@@ -0,0 +1,189 @@
+const logotext = "What,s Up Buddy?";
+const meta = {
+  title: "Aveeral Gaur",
+  description:
+    "I'm Aveeral, a MERN Stack developer and I am a programmer with good habits.",
+};
+
+const introdata = {
+  title: "Hi, I'm Aveeral Gaur",
+  animated: {
+    first: "Developer",
+    second: "Engineer",
+    third: "Enthusiast",
+    fourth: "Programmer",
+    fifth: "Visionary",
+    sixth: "And I am not a geek",
+  },
+  description:
+    "Dedicated MERN Stack developer and enthusiast with an insatiable curiosity for unraveling the complexities of web development.",
+  your_img_url: "https://i.ibb.co/wRjcnGL/me.jpg",
+};
+
+const dataabout = {
+  title: "Who am I?",
+  aboutme:
+    "I have 1.5 years of experience working as MERN developer. I am well versed in web-development using this popular tech stack and it's frameworks. I contributed my work in variety of projects and tasks. I have experience in building micro-services. I am proficient in working with Javascript,Nodejs,NestJs,Typescript,MySQl,MongoDb. I have ability to convey thoughts and ideas not just on paper but also on screens.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Backend Developer",
+    where: "Digipay Vault Fintech",
+    date: "Feb-2023 to Present",
+  },
+  {
+    jobtitle: "Associate Software Engineer",
+    where: "Oriserve",
+    date: "Sept-2022 to Feb-2022",
+  },
+  {
+    jobtitle: "Web Development Intern",
+    where: "OkayGO",
+    date: "July-2021 to Aug-2021",
+  },
+];
+const projects = [
+  {
+    projectTitle: "MERN Stack Website Management(rideNrepair)",
+  },
+  {
+    projectTitle: "Admin Dashboard",
+  },
+  {
+    projectTitle: "Automatic Number Plate Recognition System",
+  },
+];
+const skills = [
+  {
+    name: "NodeJs",
+  },
+
+  {
+    name: "Typescript",
+  },
+
+  {
+    name: "Javascript",
+  },
+
+  {
+    name: "NestJs",
+  },
+
+  {
+    name: "MySQL",
+  },
+
+  {
+    name: "Mongodb",
+  },
+
+  {
+    name: "Data Structures",
+  },
+
+  {
+    name: "Git",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "aveeralgaur@gmail.com",
+  description: "Looking forward to hearing from you!",
+  YOUR_SERVICE_ID: "service_zwqljbr",
+  YOUR_TEMPLATE_ID: "template_410n83f",
+  YOUR_USER_ID: "lQSbXELDQ4Q2sRiDe",
+};
+
+const socialprofils = {
+  github: "https://github.com/aveeralgaur",
+  linkedin: "https://www.linkedin.com/in/aveeral-gaur-791574244",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  projects,
+  skills,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
